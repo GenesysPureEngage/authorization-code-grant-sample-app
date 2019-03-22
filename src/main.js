76856c1cd1025e633c04f0901a233ff8d37dc9ca
Ekaterina Malygina <ekaterina.malygina@genesys.com>
2019-03-22 22:26:09 +0300

    Fix apiUri

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const path = require('path');
-const url = require('url');
 const workspace = require('genesys-workspace-client-js');
-const authentication = require('genesys-authentication-client-js');
-
-let request = require('request');
-
 const app = express();
 const routes = express.Router();
 
-const apiUrl = "<apiUrl>";
+const workspaceApiUrl = "<workspaceApiUrl>";
+const authApiUrl = "<authApiUrl>";
 const apiKey = "<apiKey>";
 const clientId = "<clientId>";
 const port = 8080;
@@ -21,55 +15,46 @@ const state = {
     current: 'Logged out'
 };
 
-const authClient = new authentication.ApiClient();
-authClient.basePath = `${apiUrl}/auth/v3`;
-authClient.defaultHeaders = {
-  'x-api-key': apiKey
-};
-
-const workspaceApi = new workspace(apiKey, apiUrl);
+const workspaceClient = new workspace(apiKey, workspaceApiUrl);
 
 routes.get('/info', (req, res) => {
-    const user = workspaceApi.user;
+    const user = workspaceClient.user;
     const value = {
         state: state.current,
         user: user
     };
-    
     res.send(value);
 });
 
 routes.get('/initialize', (req, res) => {
     const code = req.query.code;
-    
     state.current = 'Initializing';
     state.code = code;
-    
-    workspaceApi.initialize({code: code, redirectUri: state.redirectUri}).then( () => {
+    workspaceClient.initialize({code: code, redirectUri: state.redirectUri}).then(() => {
         state.current = 'Logged in';
     }).catch(err => {
         console.error(err);
         state.current = 'Error';
     });
-    
+
     res.redirect('/');
 });
 
 routes.get('/login', (req, res) => {
     const redirectUri = `${req.protocol}://${req.hostname}:${port}/initialize`;
     state.redirectUri = redirectUri;
-    
-    const authLoginPage = `${apiUrl}/auth/v3/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+
+    const authLoginPage = `${authApiUrl}/auth/v3/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
     res.redirect(authLoginPage);
 });
 
 routes.post('/logout', (req, res) => {
     state.current = 'Logged out';
-    workspaceApi.destroy();
+    workspaceClient.destroy();
 });
 
 routes.get('/*', (req, res) => {
-   res.redirect('/');
+    res.redirect('/');
 });
 
 app.use(express.static('public', {
@@ -78,7 +63,7 @@ app.use(express.static('public', {
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(routes);
 
 app.listen(port, () => {
